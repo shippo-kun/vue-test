3b55ba104b8247af09ac135cf1907599ba480dc7
shippokun <okashi0tabetai@gmail.com>
2019-09-06 01:07:20 +0900

    test: about input value

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -12,4 +12,11 @@ describe('HelloWorld.vue', () => {
     const inputValue = wrapper.vm.$data.inputValue;
     expect(inputValue).toBe('');
   });
+  it('inputのvalueが変化すれば、バインディングされたデータも共に変化', () => {
+    const wrapper = shallowMount(HelloWorld);
+    const input = wrapper.findAll('[data-test="input"]');
+    input.setValue('Python is the best language');
+    const inputValue = wrapper.vm.$data.inputValue;
+    expect(inputValue).toBe('Python is the best language');
+  });
 });
